15fab368c9c5b8b5f4ea125dbe1963891b535acf
lozier <latayroutier@gmail.com>
2022-09-21 15:29:02 -0400

    added eventlisteners to each clue when mousedown

--- index.js
+++ index.js
@@ -15,6 +15,78 @@ ctx = game.getContext('2d');
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight
 
+const polaroidCoord = {
+    dHeight: 40,
+    dWidth: 40,
+    positionX: 600,
+    positionY: 45  
+}
+
+const featherCoord = {
+    dHeight: 40,
+    dWidth: 40,
+    positionX: 15,
+    positionY: 300  
+}
+
+const tracksCoord = {
+    dHeight: 40,
+    dWidth: 40,
+    positionX: 500,
+    positionY: 600  
+}
+
+const bloodCoord = {
+    dHeight: 40,
+    dWidth: 40,
+    positionX: 350,
+    positionY: 370  
+}
+
+const polaroidArea = {
+    x: {
+        start: polaroidCoord.positionX,
+        end: polaroidCoord.positionX + polaroidCoord.dWidth
+    },
+    y: {
+        start: polaroidCoord.positionY,
+        end: polaroidCoord.positionY + polaroidCoord.dHeight
+    }
+}
+
+const featherArea = {
+    x: {
+        start: featherCoord.positionX,
+        end: featherCoord.positionX + featherCoord.dWidth
+    },
+    y: {
+        start: featherCoord.positionY,
+        end: featherCoord.positionY + featherCoord.dHeight
+    }
+}
+
+const tracksArea = {
+    x: {
+        start: tracksCoord.positionX,
+        end: tracksCoord.positionX + tracksCoord.dWidth
+    },
+    y: {
+        start: tracksCoord.positionY,
+        end: tracksCoord.positionY + tracksCoord.dHeight
+    }
+}
+
+const bloodArea = {
+    x: {
+        start: bloodCoord.positionX,
+        end: bloodCoord.positionX + bloodCoord.dWidth
+    },
+    y: {
+        start: bloodCoord.positionY,
+        end: bloodCoord.positionY + bloodCoord.dHeight
+    }
+}
+
 //using drawImage to get images to play nice with canvas
 makePolaroid();
 
@@ -23,7 +95,13 @@ function makePolaroid()
   polaroid = new Image();
   polaroid.src = 'images/polaroid.png';
   polaroid.onload = function(){
-    ctx.drawImage(polaroid, 0, 0, 30, 30, 600, 45, 40, 40);
+    ctx.drawImage(
+        polaroid,
+        polaroidCoord.positionX,
+        polaroidCoord.positionY,
+        polaroidCoord.dWidth,
+        polaroidCoord.dHeight
+    );
   }
 //     addEventListener('mousedown', (event) => {
 //     alert('clue clicked')
@@ -37,7 +115,13 @@ function makeFeather()
   feather = new Image();
   feather.src = 'images/feather copy.png';
   feather.onload = function(){
-    ctx.drawImage(feather, 0, 0, 30, 30, 15, 300, 40, 40);
+    ctx.drawImage(
+        feather,
+        featherCoord.positionX,
+        featherCoord.positionY,
+        featherCoord.dWidth,
+        featherCoord.dHeight
+    )
   }
 }
 
@@ -48,7 +132,13 @@ function makeTracks()
   tracks = new Image();
   tracks.src = 'images/Mothman Track-1.png.png';
   tracks.onload = function(){
-    ctx.drawImage(tracks, 0, 0, 30, 30, 500, 600, 40, 40);
+    ctx.drawImage(
+        tracks,
+        tracksCoord.positionX,
+        tracksCoord.positionY,
+        tracksCoord.dWidth,
+        tracksCoord.dHeight
+    );
   }
 }
 
@@ -59,7 +149,13 @@ function makeBlood()
   blood = new Image();
   blood.src = 'images/blood stain.png';
   blood.onload = function(){
-    ctx.drawImage(blood, 0, 0, 30, 30, 350, 370, 40, 40);
+    ctx.drawImage(
+        blood,
+        bloodCoord.positionX,
+        bloodCoord.positionY,
+        bloodCoord.dWidth,
+        bloodCoord.dHeight
+    )
   }
 }
 
@@ -67,26 +163,27 @@ const printMousePos = (game, event) => {
     const rect = game.getBoundingClientRect()
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
-    console.log('x: ' + x + ' y: ' + y) 
+    return { x: x, y: y }
 }
+
 game.addEventListener('mousedown', function(e) {
-    printMousePos(game,e)
+    const coordinate = printMousePos(game,e);
+    console.log(coordinate) 
+
+    if((polaroidArea.x.start <= coordinate.x && coordinate.x <= polaroidArea.x.end) 
+    && (polaroidArea.y.start <= coordinate.y && coordinate.y <= polaroidArea.y.end)) {
+        console.log('photo area clicked')
+    } else if ((featherArea.x.start <= coordinate.x && coordinate.x <= featherArea.x.end)
+    && (featherArea.y.start <= coordinate.y && coordinate.y <= featherArea.y.end)) {
+        console.log('feather area clicked')
+    } else if ((tracksArea.x.start <= coordinate.x && coordinate.x <= tracksArea.x.end) 
+    && (tracksArea.y.start <= coordinate.y && coordinate.y <= tracksArea.y.end)) {
+        console.log('tracks area clicked')
+    } else if ((bloodArea.x.start <= coordinate.x && coordinate.x <= bloodArea.x.end) 
+    && (bloodArea.y.start <= coordinate.y && coordinate.y <= bloodArea.y.end)) {
+        console.log('blood area clicked')
+    }
 })
-
-
-// function simulateClick(target, x, y) {
-//     let clickEvent = new MouseEvent(
-//         "click",
-//         {
-//             clientX: x,
-//             clientY: y,
-//         }
-//     );
-//     target.dispatchEvent(clickEvent);
-    
-    
-// }
-// simulateClick(game, 600, 45);
 
 game.setAttribute('width', getComputedStyle(game)['width'])
 game.setAttribute('height', getComputedStyle(game)['height'])
