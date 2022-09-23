a61360d94664e4886c7a40d1d69153c2a372c34f
lozier <latayroutier@gmail.com>
2022-09-23 09:31:55 -0400

    added mothman image and a few attempts at enemy mechanics

--- index.js
+++ index.js
@@ -16,13 +16,19 @@ const foundPic = document.getElementById('moth-polaroid')
 const foundTracks = document.getElementById('moth-tracks')
 const foundBlood = document.getElementById('moth-blood')
 
+// const mothmanTwo = document.getElementById('take2')
+
+// const darkness = document.getElementById('flashlight')
+// ctx = darkness.getContext('2d')
+
 const game = document.getElementById('canvas'),
 ctx = game.getContext('2d');
 // getting true height and width of canvas
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
-// flashlight effect
+
+// CURRENT FUNCTIONAL FLASHLIGHT EFFECT
 game.addEventListener('mousemove', function(e) {
     x = e.offsetX;
     y = e.offsetY;
@@ -45,6 +51,62 @@ game.addEventListener('mousemove', function(e) {
     ctx.closePath();
 
 })
+
+// ATTEMPTED TO PUT A SECOND CANVAS OVER FIRST CANVAS TO HOLD FLASHLIGHT EFFECT
+
+// darkness.addEventListener('mousemove', function(e) {
+//     x = e.offsetX;
+//     y = e.offsetY;
+//     radius = 150;
+//     darkCtx.globalCompositeOperation = 'source-over';
+//     darkCtx.fillStyle = 'black'
+//     darkCtx.clearRect(0, 0, darkCtx.canvas.width, darkCtx.canvas.height);
+//     darkCtx.fillRect(0,0, darkCtx.canvas.width, darkCtx.canvas.height);
+
+//     darkCtx.beginPath();
+//     radialGradient = darkCtx.createRadialGradient(x, y, 1, x, y, radius);
+//     radialGradient.addColorStop(0, 'rgba(255,255,255,1)');
+//     radialGradient.addColorStop(1, 'rgba(0,0,0,0)');
+  
+//     darkCtx.globalCompositeOperation = "destination-out";
+  
+//     darkCtx.fillStyle = radialGradient;
+//     darkCtx.arc(x, y, radius, 0, Math.PI*2, false);
+//     darkCtx.fill();
+//     darkCtx.closePath();
+
+// })
+
+// ATTTEMPT TO CREATE FLASHLIGHT EFFECT WITH CSS
+
+// let mouseX = 0;
+// let mouseY = 0;
+// let flashlight = document.getElementById('flashlight');
+
+// const getMousePosition = (e) => {
+//     mouseX = [0].pageX;
+//     mouseY = [0].pageY;
+
+//     flashlight.style.setProperty("--Xpos", mouseX + "px");
+//     flashlight.style.setProperty("--Ypos", mouseY + "px");
+// }
+
+// document.addEventListener("mousemove", getMousePosition);
+
+// function trackMouse(event) {
+//     document.documentElement.style.setProperty(
+//       '--cursorXPos',
+//       `${event.clientX}px`
+//     )
+//     document.documentElement.style.setProperty(
+//       '--cursorYPos',
+//       `${event.clientY}px`
+//     )
+//   }
+  
+//   document.addEventListener('mousemove', trackMouse)
+
+// OBFECTS TO HOLD COORDINATES OF CLUES FOR EASY REFERENCE
 
 const polaroidCoord = {
     dHeight: 45,
@@ -73,6 +135,8 @@ const bloodCoord = {
     positionX: 670,
     positionY: 585 
 }
+
+//CALCULATES AREA OF ENTIRE CLUE SO PLAYER DOES NOT HAVE TO CLICK THE EXACT CENTER TO COLLECT IT
 
 const polaroidArea = {
     x: {
@@ -187,7 +251,8 @@ function makeBlood()
   }
 }
 
-// get coordinates of area clicked
+
+// GETS COORDINATES OF MOUSE CLICK
 const printMousePos = (game, event) => {
     const rect = game.getBoundingClientRect()
     const x = event.clientX - rect.left
@@ -196,7 +261,7 @@ const printMousePos = (game, event) => {
 }
 
 
-// eventListener for clues
+// DETECTS WHEN CLUE IS CLICKED AND REVEALS HIDDEN CLUE IN CLUE BOX
 game.addEventListener('mousedown', function(e) {
     const coordinate = printMousePos(game,e);
     console.log(coordinate) 
@@ -221,6 +286,8 @@ game.addEventListener('mousedown', function(e) {
     }
 })
 
+// ATTEMPT TO ADD SOUND (CURRENTLY ABANDONED)
+
 // class sound (src) {
 //     this.sound = document.createElement("audio");
 //     this.sound.src = src;
@@ -237,40 +304,40 @@ game.addEventListener('mousedown', function(e) {
 // }
 
 
-
+// GETS TRUE CANVAS SIZE 
 game.setAttribute('width', getComputedStyle(game)['width'])
 game.setAttribute('height', getComputedStyle(game)['height'])
 
-
+/////////ATTEMPT TO PUT 'MOTHMAN' AS A SQUARE ON CANVAS LAYER/////
 
 // create a class for Mothman
-class Mothman {
-    constructor(x, y, color, width, height) {
-        this.x = x,
-        this.y = y;
-        this.color = color;
-        this.width = width;
-        this.height = height,
-        this.speed = 10,
-        this.render = () => {
-            ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this.width, this.height)
-        }
-    }
-}
+// class Mothman {
+//     constructor(x, y, color, width, height) {
+//         this.x = x,
+//         this.y = y;
+//         this.color = color;
+//         this.width = width;
+//         this.height = height,
+//         this.speed = 10,
+//         this.render = () => {
+//             ctx.fillStyle = this.color
+//             ctx.fillRect(this.x, this.y, this.width, this.height)
+//         }
+//     }
+// }
 
 // randomly generate Mothman's location on the board
-const randomGenMothX = (max) => {
-    return Math.floor(Math.random() * max)
-}
+// const randomGenMothX = (max) => {
+//     return Math.floor(Math.random() * max)
+// }
 
-const randomGenMothY = (max) => {
-    return Math.floor(Math.random() * max)
-}
+// const randomGenMothY = (max) => {
+//     return Math.floor(Math.random() * max)
+// }
 
-const mothman = new Mothman(randomGenMothX(game.width), randomGenMothY(game.height), 'pink', 75, 100)
+// const mothman = new Mothman(randomGenMothX(game.width), randomGenMothY(game.height), 'pink', 75, 100)
 
-
+// GAMELOOP THAT TRACKS WIN CONDITION AND CONTROLS TRY AGAIN BUTTON
 
 const gameloop = () => {
     if(foundPic.style.visibility == 'visible'
@@ -284,6 +351,7 @@ const gameloop = () => {
 
 const gameInterval = setInterval(gameloop, 60)
 
+//HOLDS STOPGAME, RE-HIDES CLUES AND TRY AGAIN BUTTON
 const stopGameLoop = () => {
     clearInterval(gameInterval)
     foundPic.style.visibility = 'hidden'
@@ -295,6 +363,7 @@ const stopGameLoop = () => {
 
 }
 
+// STARTS GAME AS BLACK ON LOAD
 document.addEventListener('DOMContentLoaded', function () {
     gameInterval
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
